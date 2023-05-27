b35c32d7fa6a9bf830cd2ef6a9905ce1965a3935
Daniel Hoh <daniel.hoh>
2023-05-27 17:22:21 +1000

    adding catchall routes for unknown routes

--- app/index.js
+++ app/index.js
@@ -1,14 +1,18 @@
+//Initialize dependencies
 const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
 const sqlite3 = require('sqlite3');
 
+//initialize express
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+//set to use body parser, for POST requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//base route
 app.get("/", (req, res) => {;
       res.send("Welcome to the project!");
 });
@@ -45,6 +49,21 @@ app.use('/user',userRoutes);
 var logRoutes = require('./routes/logRoutes');
 app.use('/log',logRoutes);
 
+//unknown route
+app.get('*', function(req, res) {
+    res.status(404).send('Error: unknown route');
+});
+app.put('*', function(req, res) {
+    res.status(404).send('Error: unknown route');
+});
+app.post('*', function(req, res) {
+    res.status(404).send('Error: unknown route');
+});
+app.delete('*', function(req, res) {
+    res.status(404).send('Error: unknown route');
+});
+
+//set listener
 app.listen(PORT, function () {;
       console.log(`Express server listening on port ${PORT}`);
 });
