44edb6bef88340d279e0b8b09e63f906c8474571
Daniel Hoh <daniel.hoh>
2023-06-03 17:15:43 +1000

    add routeing settings to bind port 8080 to 80

--- setup.sh
+++ setup.sh
@@ -20,3 +20,6 @@ npm install express --save
 npm install pm2 --save
 ##sqlite3
 npm install sqlite3 --save
+
+##set up routes for aws
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
