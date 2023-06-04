0fe32fc3dd21bb2a74267d3e5f40298abfa38875
buddhi <bagun2@student.monash.edu>
2023-06-04 18:17:14 +1000

    Fixed Git Issue, updated Alert Page.

--- app/client/build/components/alert/Options.jsx
+++ app/client/build/components/alert/Options.jsx
@@ -1,17 +1,28 @@
 const Options = () => {
     return (
         <div className="mt-4 ml-6 p-4 w-11/12 h-fit border-4 secondary-colour-border">
-            <div className="ml-3">
-                <h2 className="text-center text-lg font-medium secondary-colour-border">Plants</h2>
-                <p className="text-center tracking-widest secondary-colour-border">Carrots x 20</p>
-            
-            <h2 className="text-center text-lg font-medium secondary-colour-border">Data</h2>
-                <p className="text-center tracking-widest secondary-colour-border">Moisture Level: 95%</p>
-                <p className="text-center tracking-widest secondary-colour-border">Soil pH: 6.5pH</p>
-                <p className="text-center tracking-widest secondary-colour-border">Phosphorus Level: 22ppm</p>
-                <p className="text-center tracking-widest secondary-colour-border">Overall Plant Health: Great</p>
-            </div>
+           <h2 className="text-left mb-2 text-lg font-medium secondary-colour-border">Moisture Levels</h2>
+
+                <label className="relative inline-flex items-center mr-5 cursor-pointer">
+                    <input type="checkbox" value="" className="sr-only peer" />
+                    <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"></div>
+                </label>
+
+                <h2 className="text-left mb-2 text-lg font-medium secondary-colour-border">pH Levels</h2>
+
+                <label className="relative inline-flex items-center mr-5 cursor-pointer">
+                    <input type="checkbox" value="" className="sr-only peer" />
+                    <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"></div>
+                </label>
+
+                <h2 className="text-left mb-2 text-lg font-medium secondary-colour-border">Phosphorus Levels</h2>
+
+                <label className="relative inline-flex items-center mr-5 cursor-pointer">
+                    <input type="checkbox" value="" className="sr-only peer" />
+                    <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"></div>
+                </label>
         </div>
+        
     )
 }
 
