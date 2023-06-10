d7887c1400ffed07f37b0e3fa2abfbb178d8a1da
Daniel Hoh <daniel.hoh>
2023-06-10 12:53:09 +1000

    adding nodeemailer package

--- setup.sh
+++ setup.sh
@@ -9,6 +9,14 @@ sudo apt update
 sudo apt install nodejs
 sudo apt install npm
 
+## move into app directory
+cd app
+
+##install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm install 16.15.1
+
 # Install packages
 ##axios
 npm install axios --save
@@ -25,6 +33,11 @@ npm install firebase --save
 npm install firebase-admin --save
 ##axios
 npm install axios --save
+##nodemailer
+npm install nodemailer --save
 
 ##set up routes for aws
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
+
+##set up pm2
+pm2 start index.js
