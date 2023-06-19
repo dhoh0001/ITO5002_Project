b1c590b559a4df3106ae7f3c3f21a55f5b2ce9ca
Daniel Hoh <daniel.hoh>
2023-06-19 22:34:23 +1000

    fixing sign in to sign up

--- app/app/user/signup/page.jsx
+++ app/app/user/signup/page.jsx
@@ -93,7 +93,7 @@ const Signup = () => {
                     </div>
 
                     <div>
-                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</button>
                     </div>
                     </form>
                 </div>
