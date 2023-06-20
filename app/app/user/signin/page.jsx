ee8a678405449a11901a9fb8fd33897ae1d0ba48
Daniel Hoh <daniel.hoh>
2023-06-20 20:23:08 +1000

    capitalizing address

--- app/app/user/signin/page.jsx
+++ app/app/user/signin/page.jsx
@@ -44,7 +44,7 @@ const Signin = () => {
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm secondary-colour p-6">
                     <form className="space-y-6" action="#" method="POST" onSubmit={formSubmit}>
                     <div>
-                        <label className="block text-sm font-medium leading-6 text-white">Email address</label>
+                        <label className="block text-sm font-medium leading-6 text-white">Email Address</label>
                         <div className="mt-2">
                         <input id="email" name="email" type="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                         </div>
