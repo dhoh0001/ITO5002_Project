7ecb793d24a728368d55c8fa00f4f99d0a03650f
Daniel Hoh <daniel.hoh>
2023-06-21 21:55:21 +1000

    removing sensorid

--- app/components/config/SensorConfig.jsx
+++ app/components/config/SensorConfig.jsx
@@ -277,8 +277,6 @@ const SensorConfigCom = (props) => {
                 <div className="absolute z-50 m-auto top-0 bottom-0 left-0 right-0 secondary-colour sm:w-8/12 md:w-6/12 lg:w-4/12 h-fit p-4 drop-shadow-2xl"> 
                     <form className="" action="#" method="PUT" onSubmit={formCreateSubmit}>
                         <div className="text-white mx-2">
-                            <label className="block text-sm font-bold mx-2 text-white pt-4">Sensor ID</label>
-                            <input id="sensorId" name="sensorId"  className="shadow mx-2 justify-center appearance-none border rounded py-2 px-1 text-black" />
                             <label className="block text-sm font-bold mx-2 text-white pt-4">Sensor Name</label>
                             <input id="name" name="name"  className="shadow mx-2 justify-center appearance-none border rounded py-2 px-1 text-black" />
                             <label className="block text-sm font-bold mx-2 text-white pt-4">Hardware Id</label>
